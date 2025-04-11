64ee6f7c42b281c8ff7026d708703705007197f5
r3nzph <abarracosorenznino@gmail.com>
2025-04-11 19:55:17 +0800

    Update twrp_KJ5.mk

--- twrp_KJ5.mk
+++ twrp_KJ5.mk
@@ -4,6 +4,20 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Enable Virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
+
 # Inherit from KJ5 device
 $(call inherit-product, device/tecno/KJ5/device.mk)
 
