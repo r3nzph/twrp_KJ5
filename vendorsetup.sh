a58ae48a8af99d02e8ba88aaea0fef1837a50cf2
r3nzph <abarracosorenznino@gmail.com>
2025-04-11 15:58:15 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -42,52 +42,54 @@ if [ -f "$(gettop)/bootable/recovery/orangefox.cpp" ]
 then
 	echo -e "\x1b[96mSetting up OrangeFox build vars...\x1b[m"
 	if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-		export OF_MAINTAINER="nino"
-		export FOX_VERSION=R11.1_1
-		export FOX_BUILD_TYPE="unofficial"
-   		export TW_DEFAULT_LANGUAGE="en"
-                export FOX_EXTRA_LANGUAGE=0
-		export LC_ALL="C"
-	 	export OF_FLASHLIGHT_ENABLE=0
-		export ALLOW_MISSING_DEPENDENCIES=true
-		export OF_USE_GREEN_LED=0
-		export OF_HIDE_NOTCH=1
-		export OF_USE_MAGISKBOOT=1
-		export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-		export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-		export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-		export OF_NO_MIUI_PATCH_WARNING=1
-		export FOX_USE_XZ_UTILS=1
-		export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
-	    	export OF_USE_TWRP_SAR_DETECT=1
-		export OF_NO_SAMSUNG_SPECIAL=1
-		export OF_QUICK_BACKUP_LIST="/boot;/data;"
-	    	export FOX_DELETE_AROMAFM=1
-                export FOX_EXCLUDE_AROMAFM=1
-	    	export FOX_ENABLE_APP_MANAGER=1
-		export FOX_EXTREME_SIZE_REDUCTION=1
-		export FOX_REMOVE_AAPT=1
-                export FOX_EXCLUDE_NANO_EDITOR=1
-		export FOX_USE_FSCRYPT_POLICY=2
-	    	export FOX_DISABLE_EXTRA_ABOUT_PAGE=1
-                export FOX_DELETE_MAGISK_ADDON=1
-		export FOX_USE_BUSYBOX_BINARY=0
-                export FOX_DELETE_INITD_ADDON=1
-	    	export OF_KEEP_DM_VERITY=1
-	    	export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-	    	export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
-		export OF_SCREEN_H=2460
-		export OF_STATUS_H=100
-                export FOX_DONT_KEEP_LOG_HISTORY=1
-		export OF_ALLOW_DISABLE_NAVBAR=0 # We don't have physical buttons
-		export OF_STATUS_INDENT_LEFT=48
-		export OF_STATUS_INDENT_RIGHT=48
-		export OF_CLOCK_POS=0
-		export OF_SPLASH_MAX_SIZE=100
-		export OF_PATCH_AVB20=1
-                export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"
-		export FOX_RECOVERY_BOOT_PARTITION="/dev/block/by-name/boot"
-		export FOX_AB_DEVICE=1
-		export OF_DONT_PATCH_ON_FRESH_INSTALLATION=1
+		export OF_DISABLE_OTA_MENU=1
+export FOX_AB_DEVICE=1
+export FOX_VIRTUAL_AB_DEVICE=1
+export OF_DEFAULT_KEYMASTER_VERSION=4.1
+export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+export OF_MAINTAINER="r3nzph"
+export FOX_VARIANT="A12+"
+export OF_FLASHLIGHT_ENABLE=0
+
+export FOX_USE_BASH_SHELL=1
+export FOX_USE_NANO_EDITOR=1
+export FOX_USE_TAR_BINARY=1
+export FOX_USE_SED_BINARY=1
+export FOX_USE_XZ_UTILS=1
+export FOX_ASH_IS_BASH=1
+export OF_ENABLE_LPTOOLS=1
+export FOX_DELETE_MAGISK_ADDON=1
+export FOX_DELETE_AROMAFM=1
+export FOX_ENABLE_APP_MANAGER=1
+export OF_SUPPORT_VBMETA_AVB2_PATCHING=1
+
+export FOX_USE_DATA_RECOVERY_FOR_SETTINGS=1
+
+export OF_LOOP_DEVICE_ERRORS_TO_LOG=1
+
+export OF_USE_LZ4_COMPRESSION=true
+
+export OF_SCREEN_H=2400
+export OF_STATUS_H=95
+export OF_STATUS_INDENT_LEFT=48
+export OF_STATUS_INDENT_RIGHT=48
+export OF_ALLOW_DISABLE_NAVBAR=0
+export OF_CLOCK_POS=1
+
+export USE_CCACHE=1
+export CCACHE_EXEC=/usr/bin/ccache
+export CCACHE_MAXSIZE="5G"
+export CCACHE_DIR="~/ccache"
+
+if [ ! -d ${CCACHE_DIR} ];
+then
+  echo "CCACHE Directory/Partition is not mounted at \"${CCACHE_DIR}\""
+  echo "Please edit the CCACHE_DIR build variable or mount the directory."
+fi
+export LC_ALL="C"
+
+# Clone to fix build on minimal manifest
+git clone https://android.googlesource.com/platform/external/gflags/ -b android-12.1.0_r4 external/gflags
+
 	fi
 fi
