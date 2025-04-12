4ae069145b91745c9c4ca2d4fe666a21e40a3f1c
r3nzph <abarracosorenznino@gmail.com>
2025-04-12 22:56:08 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -168,9 +168,9 @@ BOARD_USE_CUSTOM_RECOVERY_FONT := "roboto_15x24.h"
 
 # Brightness Screen
 TW_NO_SCREEN_BLANK := true
-TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-TW_MAX_BRIGHTNESS := 2047
-TW_DEFAULT_BRIGHTNESS := 1200
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 150
 
 # USB Configuration
 TW_EXCLUDE_DEFAULT_USB_INIT := true
