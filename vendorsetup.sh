986b373accdd7b017da952b9b40fc23d5fdef8fb
r3nzph <abarracosorenznino@gmail.com>
2025-04-19 19:52:29 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -39,12 +39,6 @@ export CCACHE_EXEC=/usr/bin/ccache
 export CCACHE_MAXSIZE="5G"
 export CCACHE_DIR="~/ccache"
 
-if [ ! -d ${CCACHE_DIR} ];
-then
-  echo "CCACHE Directory/Partition is not mounted at \"${CCACHE_DIR}\""
-  echo "Please edit the CCACHE_DIR build variable or mount the directory."
-fi
-
 export LC_ALL="C"
 
 # Clone to fix build on minimal manifest
