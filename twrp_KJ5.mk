224bf1b5abcf4448b580708bfbe578f3b0272630
r3nzph <abarracosorenznino@gmail.com>
2025-04-21 04:12:31 +0800

    Update twrp_KJ5.mk

--- twrp_KJ5.mk
+++ twrp_KJ5.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Inherit some common twrp stuff.
+$(call inherit-product-if-exists, vendor/twrp/config/common.mk)
+
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
