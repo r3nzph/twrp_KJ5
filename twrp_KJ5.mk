4ed341391cf039143612434e3e6ddd636cfa708c
r3nzph <abarracosorenznino@gmail.com>
2025-04-24 00:08:26 +0800

    Update twrp_KJ5.mk

--- twrp_KJ5.mk
+++ twrp_KJ5.mk
@@ -5,8 +5,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
@@ -20,6 +20,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 
 # Inherit from KJ5 device
 $(call inherit-product, device/tecno/KJ5/device.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 PRODUCT_DEVICE := KJ5
 PRODUCT_NAME := twrp_KJ5
